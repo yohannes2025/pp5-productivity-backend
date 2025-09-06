1154caaf34ed0c8118067df0a80952842af07bce
yohannes2025 <johannes.m.tekle@gmail.com>
2025-09-06 12:52:50 +0200

    Commit : Add auth URLs for registration and login Subject: Add URL patterns for auth endpoints Body: Update urls.py to include paths for /api/register/ and /api/login/ using RegisterViewSet and LoginViewSet for User Story 1. This enables frontend to access authentication APIs.

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,22 +1,10 @@
-"""
-URL configuration for drf_api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# drf_api/urls.py (updated)
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from productivity_app.views import RegisterViewSet, LoginViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/register/', RegisterViewSet.as_view(), name='register'),
+    path('api/login/', LoginViewSet.as_view(), name='login'),
 ]
