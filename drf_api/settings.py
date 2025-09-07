04e9b6c95682fb686772fc43b64535991a60c926
yohannes2025 <johannes.m.tekle@gmail.com>
2025-09-07 17:01:10 +0200

    Commit : Add static files and Whitenoise config Subject : Config: Set up static files and Whitenoise Body : Configure static root and URL.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -140,6 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
